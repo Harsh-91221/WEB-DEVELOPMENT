b05a1694ec690f2ed1001469a91d1dd32fe5f35f
Harsh91221 <harshch91221@gmail.com>
2023-12-01 23:21:26 +0530

    CODE

--- SetTimeout.js
+++ SetTimeout.js
@@ -1,9 +1,23 @@
-function x() {
-    for (let i = 1; i <= 5; i++) {
-        setTimeout(function () {
-            console.log(i);
+// function x() {
+//     for (let i = 1; i <= 5; i++) {
+//         setTimeout(function () {
+//             console.log(i);
 
-        }, i*1000);
+//         }, i*1000);
+//     }
+//     console.log("Hello Ji");
+// }
+// x();
+
+//Using var only
+function x() {
+    for (var i = 1; i <= 5; i++) {
+        function close(x) {
+            setTimeout(function () {
+                console.log(x);
+            }, x * 1000)
+        }
+        close(i);
     }
     console.log("Hello Ji");
 }
