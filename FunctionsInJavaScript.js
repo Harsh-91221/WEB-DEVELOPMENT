1d37c4937e9ff53b49ad7b3a014c4c1e3d5746c0
Harsh91221 <harshch91221@gmail.com>
2023-12-13 19:59:54 +0530

    CODE

--- FunctionsInJavaScript.js
+++ FunctionsInJavaScript.js
@@ -4,30 +4,34 @@ function a()
     console.log("Hello");
 }
 a();
+
 //Function Expression
 var a=function ()
 {
     return a;
 }
 console.log(a());
-//Anonymous Function
-var a=function ()
+
+//Anonymous Function -> Function without any name
+function ()
 {
-    console.log("Bye");
+
 }
-a();
+
 //Named Function
 var a=function xyz()
 {
     console.log(a);
 }
 a();
+
 //Arguments and Parameters
 var b=function(para1,para2) // Parameters
 {
     console.log(para1,para2); 
 }
 b(1,2);  //Arguments
+
 //First class Functions aka First class citizens
 var b=function(para1)
 {
