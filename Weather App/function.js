12a89dcd49a8c1a0e91a43555037d10ac5bed153
Harsh91221 <harshch91221@gmail.com>
2024-01-03 00:42:04 +0530

    CODE

---
+++ Weather App/function.js
@@ -0,0 +1,18 @@
+const apiKey = "1c78c6124313dd2f29a9658f2aab0c8e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".Wind").innerHTML = data.wind.speed + " km/h";
+}
+if (searchBtn) {
+    searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value);
+    });
+}
