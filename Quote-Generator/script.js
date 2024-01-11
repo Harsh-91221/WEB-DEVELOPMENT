430808635cdcf79dbf2c7e70f05a5a4fedba8559
Harsh91221 <harshch91221@gmail.com>
2024-01-11 16:11:04 +0530

    CODE

---
+++ Quote-Generator/script.js
@@ -0,0 +1,14 @@
+const quote = document.getElementById("quote");
+const character = document.getElementById("character");
+const api_URL = "https://animechan.xyz/api/random";
+async function getQuote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    quote.innerHTML = data.quote;
+    character.innerHTML = data.character;
+}
+getQuote(api_URL);
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + character.innerHTMLX, "Tweet Window", "width=600", "height=300");
+}
